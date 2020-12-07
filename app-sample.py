48a26425430c3eaa9469ae860796832a22d383b6
alanthejudoka <alasal21@bergen.org>
2020-12-07 14:48:47 -0500

    start- examples

--- app-sample.py
+++ app-sample.py
@@ -10,22 +10,10 @@ socket_ = SocketIO(app, async_mode=async_mode)
 thread = None
 thread_lock = Lock()
 
-ACTIVE_CLIENTS = 0
 
 @app.route('/')
 def index():
-    return render_template('goclient.html', async_mode=socket_.async_mode)
-
-
-@socket_.on('connect')
-def on_connect():
-    global ACTIVE_CLIENTS
-    ACTIVE_CLIENTS += 1
-
-@socket_.on('disconnect')
-def on_disconnect():
-    global ACTIVE_CLIENTS
-    ACTIVE_CLIENTS -= 1
+    return render_template('index.html', async_mode=socket_.async_mode)
 
 
 @socket_.on('my_event', namespace='/test')
@@ -43,6 +31,16 @@ def test_broadcast_message(message):
          broadcast=True)
 
 
+@socket_.on('disconnect_request', namespace='/test')
+def disconnect_request():
+    @copy_current_request_context
+    def can_disconnect():
+        disconnect()
+
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': 'Disconnected!', 'count': session['receive_count']},
+         callback=can_disconnect)
 
 
 if __name__ == '__main__':
---
+++ game.py
@@ -0,0 +1,8 @@
+#number of corners per board
+BOARD_SIZE = {
+    'normal': 16,
+    'large': 324
+}
+
+def __init__(self):
+    self.board = list(324)
