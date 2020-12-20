df30d4a34d247958abfb3b2794821e17c6e834ad
alanthejudoka <alasal21@bergen.org>
2020-12-19 22:47:36 -0500

    start- examples

--- app.py
+++ app.py
@@ -1,25 +1,50 @@
-from flask import Flask, render_template, render_template, request
+from flask import Flask, render_template, request, session, url_for, redirect
 from flask_socketio import SocketIO, send, emit, join_room, rooms
 from coolname import generate_slug
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
+bootstrap = Bootstrap(app)
 socketio = SocketIO(app)
+moment = Moment(app)
 
 
-@app.route('/')
-def hello_world():
-    return render_template("index.html")
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def get_name():
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+
+    return render_template('username.html', form=form, name=session.get('name'))
+
+
+@app.route('/index')
+def index():
+    name = session.get('name')
+    return render_template('menu.html', name=name)
 
 
 @app.route('/seek/<room_id>')
 def seek(room_id):
-    return render_template("seek.html")
+    name = session.get('name')
+    return render_template("red.html", name=name)
 
 
 @app.route('/hide/<room_id>')
 def hide(room_id):
-    return render_template("hide.html")
+    name = session.get('name')
+    return render_template("yellow.html", name=name)
 
 
 @socketio.on('message', namespace="/hide")
@@ -31,6 +56,7 @@ def join_room_flask_hide(msg):
 @socketio.on('message', namespace="/seek")
 def join_room_flask_seek(msg):
     join_room(msg)
+    send("red player has joined the game.", room=msg, namespace="/hide")
 
 
 @socketio.on('ss', namespace="/seek")
